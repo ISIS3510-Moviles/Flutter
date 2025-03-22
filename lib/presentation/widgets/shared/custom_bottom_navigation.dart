d16d6a5170a49ec650001306b18ad5f996e30615
ddi4z <d.diazm@uniandes.edu.co>
2025-03-21 19:49:34 -0500

    feature/implement-reservations-screen-#96

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -12,18 +12,17 @@ class CustomBottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-
       elevation: 0,
       currentIndex: childView.currentIndex,
-      onTap: (value) => print(value),
-      items: [
+      onTap: (index) => childView.goBranch(index),
+      items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max_outlined),
           label: 'Home'
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.favorite_outline),
-          label: 'Favorites'
+          label: 'Reservations'
         ),
       ],
     );
